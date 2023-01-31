1af82cfe147ad2b74e8bcee8a94e94ee373e4e9b
seerj30 <33357726+seerj30@users.noreply.github.com>
2023-01-31 17:36:18 +0200

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,10 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 
 @Component({
-  standalone: true,
-  imports: [MatButtonModule, MatCardModule],
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
+  standalone: true,
+  imports: [MatButtonModule, MatCardModule],
 })
 export class AppComponent {}
